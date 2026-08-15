e90c01f84818a0fc72e017a4f3f9d436210096d1
Chris Lloyd <192208532+ChrisLloydME@users.noreply.github.com>
2026-08-15 22:12:20 +0800

    Initial Commit

---
+++ Mox/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mox
+//
+//  Created by Christopher Lloyd on 2026.08.15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ MoxTests/MoxTests.swift
@@ -0,0 +1,19 @@
+//
+//  MoxTests.swift
+//  MoxTests
+//
+//  Created by Christopher Lloyd on 2026.08.15.
+//
+
+import Testing
+@testable import Mox
+
+struct MoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
